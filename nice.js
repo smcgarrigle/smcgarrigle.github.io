88d627ca37fb3ab2f1bc5385fd7f31bc6f20b173
smcgarrigle <stephen_mcgarrigle@wired.com>
2017-05-19 17:04:50 -0700

    trimmed js out

--- nice.js
+++ nice.js
@@ -14,10 +14,16 @@ function() {
 
 
 javascript: void
+
 function() {
-    (function() {
-        var content = document.querySelector("meta[name='id']").getAttribute("content");
-        var gotoURL = "https://ci-copilot.aws.conde.io/wrd/articles/" + content;
-        window.open(gotoURL);
-    })()
+	(function() {
+		var o = document.querySelector("meta[name='id']").getAttribute("content");
+		n = "https://ci-copilot.aws.conde.io/wrd/articles/" + o,
+		c = window.location.protocol "//"
+		window.location.host n;
+		window.open(c)
+	})()
 }();
+
+
+javascript:void function()%7B(function()%7Bvar o%3Ddocument.querySelector("body").getAttribute("class"),t%3D/postid-(%5Cd%2B)/,i%3Dt.exec(o)%5B1%5D,n%3D"/wp-admin/post.php%3Fpost%3D"%2Bi%2B"%26action%3Dedit",c%3Dwindow.location.protocol%2B"//"%2Bwindow.location.host%2Bn%3Bwindow.open(c)%7D)()%7D()%3B
